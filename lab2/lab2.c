47290c3c5cb4ddcf23139b367860912d50efea35
allexkap <aekpstn@gmail.com>
2021-11-08 12:55:24 +0300

    add details to .c (lab2)

--- lab2/lab2.c
+++ lab2/lab2.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 
+// xor swap
 void swap(char *a, char *b) {
     *a ^= *b;
     *b ^= *a;
@@ -7,20 +8,25 @@ void swap(char *a, char *b) {
 }
 
 int main() {
+    // input
     int x = 0xDEADBEEF;
-    printf("%X\n", x);
 
+    // vars
     char size = sizeof(x);
     char amount[size];
 
+    // show binary
+    for (char i = size*8-1; i >= 0; i--) printf("%d", (x&1<<i)!=0); printf("\n");
+
+    // calc params
     for (char i = 0; i < size; i++) {
+        amount[i] = 0;
         for (char j = 0; j < 8; j++) {
             amount[i] += (x&(1<<8*i+j))!=0;
         }
     }
 
-    for (char i = 0; i < size; i++) printf("%d ", amount[i]); printf("\n");
-
+    // bubble sort
     for (char k = 0; k < size; k++) {
         for (char i = 0; i < size - 1; i++) {
             if (amount[i] > amount[i+1]) {
@@ -30,8 +36,9 @@ int main() {
         }
     }
 
-    for (char i = 0; i < size; i++) printf("%d ", amount[i]); printf("\n");
+    // show binary
+    for (char i = size*8-1; i >= 0; i--) printf("%d", (x&1<<i)!=0); printf("\n");
 
-    printf("%X\n", x);
+    // exit
     return 0;
 }
