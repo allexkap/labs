7e6febda0d263fae3e563018d4cbd85fd65a88bc
allexkap <aekpstn@gmail.com>
2022-02-09 14:18:51 +0300

    base complete (clab)

--- clab/base.cpp
+++ clab/base.cpp
@@ -1,47 +1,48 @@
 class String {
 
-  private:
-    char *pointer;
+  public:
+    char *pointer = nullptr;
     int length;
 
-  public:
-    String() {
-        cout << "none\n";
+    void empty() {
+        if (!pointer) return;
+        delete [] pointer;
+        pointer = nullptr;
         length = 0;
-        pointer = 0;
+    }
+
+    String() {
+        empty();
     }
     String(string str) {
-        cout << "string\n";
         length = str.length();
-        pointer = new char [length];
-        for (int i = 0; i < length; i++) pointer[i] = str[i];
+        if (length) {
+            pointer = new char [length+1];
+            for (int i = 0; i < length; i++) pointer[i] = str[i];
+            pointer[length] = '\0';
+        }
     }
-    String(char chr) : String(string(1, chr)) {
-        cout << "char\n";
-    }
-    String(const String &Str) {
-        cout << "copy\n";
+    String(char chr) : String(string(1, chr)) { }
+    String(String &Str) {
         length = Str.length;
-        pointer = new char [length];
-        for (int i = 0; i < length; i++) pointer[i] = Str.pointer[i];
+        if (length) {
+            pointer = new char [length+1];
+            for (int i = 0; i < length; i++) pointer[i] = Str.pointer[i];
+            pointer[length] = '\0';
+        }
     }
     ~String() {
-        cout << "end\n";
-        if (pointer) delete [] pointer;
+        empty();
     }
 
     char read(int pos) {
         return pointer[pos];
     }
     void show() {
-        for (int i = 0; i < length; i++) cout << pointer[i];
-        cout << endl;
+        cout << "length: " << length << endl;
+        if (pointer) {
+            cout << "pointer: " << &pointer << endl;
+            cout << "value: " << pointer << endl;
+        }
     }
 };
-
-int main() {
-    String tmp = String("abc");
-    String str(tmp);
-    str.show();
-    return 0;
-}
